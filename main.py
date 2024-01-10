bc6af250dc43464c256f5772b2bea82cdb2e1d41
Aflah <aflah20082@iiitd.ac.in>
2024-01-10 19:13:15 +0530

    Migrated to Pages

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
 import streamlit as st
 import os
 
-# Get the list of images
-image_options = os.listdir('accuracy_plots_peft_methods/')
+st.set_page_config(
+    page_title="Main",
+    page_icon="🎯",
+)
 
-st.title('Accuracy Plots for PEFT Methods')
+st.title('Plots for Memorization Experiments')
 
-string_lens = [16,32,64,128,256,512,1024]
-methods = ['full-finetuning', 'ia3', 'lora', 'p-tuning', 'prefix-tuning', 'prompt-tuning']
-suffixes = ['', 'Target modules- q_proj,v_proj Feed forward modules- down_proj', 
-            'r- 8 alpha- 32 dropout- 0.1', 'Encoder hidden size- 128 Num virtual tokens- 8',
-            'Num virtual tokens- 8 Init text- This is a test']
-string_types = ['random_strings', 'non_random_strings']
-
-chosen_string_len = st.multiselect('Choose string length', string_lens)
-chosen_method = st.multiselect('Choose method', methods)
-chosen_type = st.multiselect('Choose string type', string_types)
-
-suffixes_for_chosen_methods = []
-for method in chosen_method:
-    suffixes_for_chosen_methods.append(suffixes[methods.index(method)])
-
-def build_file_name(string_len, method, string_type, suffix):
-    base = 'accuracy_plots_peft_methods/accuracy_' + string_type + ' pythia-1b ' + method + ' ' + str(string_len) + 'x1 Epochs- 100 BSize- 1 Seed- 42'
-    if suffix != '':
-        base += ' ' + suffix
-    base += '.png'
-    return base
-
-# sort string lengths by first converting to int
-chosen_string_len = sorted(chosen_string_len, key=lambda x: int(x))
-
-for method, suffix in zip(chosen_method, suffixes_for_chosen_methods):
-    for string_type in chosen_type:
-        for string_len in chosen_string_len:
-            file_name = build_file_name(string_len, method, string_type, suffix)
-            title = file_name
-            # remove accuracy_plots_peft_methods/accuracy_ from start
-            title = title[len('accuracy_plots_peft_methods/accuracy_'):]
-            # remove .png from end
-            title = title[:-len('.png')]
-            st.markdown('#### ' + title)
-            st.image(file_name, use_column_width=True)
-            
-
+st.markdown("This is a Streamlit dashboard for visualizing the results of the memorization experiments.")
+st.markdown("Different pages (accessible from the sidebar) contain results for different experiments.")
---
+++ pages/2_2️⃣_Freezing_Subset_Loss_Curves.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import plotly
+import plotly.express as px
+
+def main():
+    st.title("Loss Comparison")
+    df = pd.read_csv("data/loss_data_freezing_plots.csv")
+    cols = df.columns.tolist()
+    # col name must contain unfreeze
+    cols = [x for x in cols if "unfreeze" in x]
+    df = df[cols]
+    # remove cols with loss__MAX or loss__MIN in name
+    cols = [x for x in cols if "MAX" not in x and "MIN" not in x]
+    df = df[cols]
+    
+    # remove  - eval/loss from end of col name
+    cols = [x.replace(" - eval/loss", "") for x in cols]
+    df.columns = cols
+    # split colname on - and keep -1
+    cols = [x.split("-")[-1] for x in cols]
+    df.columns = cols
+    
+    # sort cols by int value
+    cols = sorted(cols, key=lambda x: int(x))
+    df = df[cols]
+
+    # get 16 random colors
+    all_colors = px.colors.qualitative.Plotly + px.colors.qualitative.D3 + px.colors.qualitative.G10 + px.colors.qualitative.T10 + px.colors.qualitative.Alphabet + px.colors.qualitative.Dark24
+    colors = all_colors[:len(cols)]
+
+    mapping = {}
+    for i, col in enumerate(cols):
+        mapping[col] = colors[i]
+
+    select_custom = st.checkbox("Select Custom")
+    if select_custom:
+        selected = st.multiselect("Select Runs", cols, default=cols)
+    else:
+        selected = []
+        select_first_k = st.checkbox("Select first k")
+        select_last_k = st.checkbox("Select last k")
+        if select_first_k:
+            k = st.slider("Select first k", 1, len(cols), 3)
+            selected.extend(cols[:k])
+        if select_last_k:
+            k = st.slider("Select last k", 1, len(cols), 3)
+            selected.extend(cols[-k:])
+        selected = list(set(selected))
+        selected = sorted(selected, key=lambda x: int(x))
+
+
+
+    colors_selected = [mapping[x] for x in selected]
+    # sort selected by int value
+    selected = sorted(selected, key=lambda x: int(x))
+    # plot line chart
+    fig = px.line(df, y=selected, x=df.index, color_discrete_map=mapping)
+    # set x and y axis labels
+    fig.update_layout(
+        xaxis_title="Epoch",
+        yaxis_title="Loss",
+    )
+    # change legend title to Layer
+    fig.update_layout(legend_title_text='Layer')
+    st.plotly_chart(fig)
+    
+if __name__ == "__main__":
+    main()
