7a7504a14e344b5b1059308936366fbfbaa829e3
Aflah <aflah20082@iiitd.ac.in>
2024-01-11 14:33:13 +0530

    New Logs

--- pages/5_5️⃣_Memorizing_Using_Intermediate_Checkpoints.py
+++ pages/5_5️⃣_Memorizing_Using_Intermediate_Checkpoints.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Memorizing Using Intermediate Checkpoints", page_icon="5️⃣")
 
@@ -17,94 +18,67 @@ def main():
     else:
         df = st.session_state.page_5_data
 
-    str_types = ['random_strings']
+    str_types = ['non_random_strings', 'random_strings']
     sizes = [16, 32, 64, 128, 256, 512, 1024]
     ckpts = [0,1,4,16,64,256,1000, 2000, 10000, 50000, 100000, 143000]
     # for i in range(2000, 144000, 1000):
     #     ckpts.append(i)
     
-    for str_type in str_types:
-        for size in sizes:
-            col_names = [get_col_name(str_type, size, ckpt) for ckpt in ckpts]
-            subset_df = df[col_names]
-            # plot
-            fig = px.line(subset_df, y=col_names, x=subset_df.index)
-            # set x and y axis labels
-            fig.update_layout(
-                xaxis_title="Epoch",
-                yaxis_title="Loss",
-            )
-            # change name in legend
-            fig.for_each_trace(lambda t: t.update(name=t.name.split(" ")[1]))
+    str_type = 'non_random_strings'
+    for size in sizes:
+        col_names = [get_col_name(str_type, size, ckpt) for ckpt in ckpts]
+        subset_df = df[col_names]
+        # plot
+        fig = plt.figure()
 
-            # title
-            fig.update_layout(title=f"Loss for {str_type} of size {size}")
+        data = subset_df.values
 
-            # change legend title to Layer
-            fig.update_layout(legend_title_text='Layer')
-            st.plotly_chart(fig)
+        # plot each column
+        for i in range(data.shape[1]):
+            plt.plot(data[:, i], label=subset_df.columns[i].split(" ")[1])
 
+        # set x and y axis labels
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
 
+        # title
+        plt.title(f"Loss for {str_type} of size {size}")
 
+        # change legend title to Layer
+        plt.legend(title='Layer')
 
-    # cols = df.columns.tolist()
-    # # col name must contain unfreeze
-    # cols = [x for x in cols if "unfreeze" in x]
-    # df = df[cols]
-    # # remove cols with loss__MAX or loss__MIN in name
-    # cols = [x for x in cols if "MAX" not in x and "MIN" not in x]
-    # df = df[cols]
-    
-    # # remove  - eval/loss from end of col name
-    # cols = [x.replace(" - eval/loss", "") for x in cols]
-    # df.columns = cols
-    # # split colname on - and keep -1
-    # cols = [x.split("-")[-1] for x in cols]
-    # df.columns = cols
-    
-    # # sort cols by int value
-    # cols = sorted(cols, key=lambda x: int(x))
-    # df = df[cols]
+        # reduce legend size
+        plt.legend(prop={'size': 6})
 
-    # # get 16 random colors
-    # all_colors = px.colors.qualitative.Plotly + px.colors.qualitative.D3 + px.colors.qualitative.G10 + px.colors.qualitative.T10 + px.colors.qualitative.Alphabet + px.colors.qualitative.Dark24
-    # colors = all_colors[:len(cols)]
+        st.pyplot(fig)
 
-    # mapping = {}
-    # for i, col in enumerate(cols):
-    #     mapping[col] = colors[i]
+    str_type = 'random_strings'
+    for size in sizes:
+        col_names = [get_col_name(str_type, size, ckpt) for ckpt in ckpts]
+        subset_df = df[col_names]
+        # plot
+        fig = plt.figure()
 
-    # select_custom = st.checkbox("Select Custom")
-    # if select_custom:
-    #     selected = st.multiselect("Select Runs", cols, default=cols)
-    # else:
-    #     selected = []
-    #     select_first_k = st.checkbox("Select first k")
-    #     select_last_k = st.checkbox("Select last k")
-    #     if select_first_k:
-    #         k = st.slider("Select first k", 1, len(cols), 3)
-    #         selected.extend(cols[:k])
-    #     if select_last_k:
-    #         k = st.slider("Select last k", 1, len(cols), 3)
-    #         selected.extend(cols[-k:])
-    #     selected = list(set(selected))
-    #     selected = sorted(selected, key=lambda x: int(x))
+        data = subset_df.values
 
+        # plot each column
+        for i in range(data.shape[1]):
+            plt.plot(data[:, i], label=subset_df.columns[i].split(" ")[1])
 
+        # set x and y axis labels
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
 
-    # colors_selected = [mapping[x] for x in selected]
-    # # sort selected by int value
-    # selected = sorted(selected, key=lambda x: int(x))
-    # # plot line chart
-    # fig = px.line(df, y=selected, x=df.index, color_discrete_map=mapping)
-    # # set x and y axis labels
-    # fig.update_layout(
-    #     xaxis_title="Epoch",
-    #     yaxis_title="Loss",
-    # )
-    # # change legend title to Layer
-    # fig.update_layout(legend_title_text='Layer')
-    # st.plotly_chart(fig)
-    
+        # title
+        plt.title(f"Loss for {str_type} of size {size}")
+
+        # change legend title to Layer
+        plt.legend(title='Layer')
+
+        # reduce legend size
+        plt.legend(prop={'size': 6})
+
+        st.pyplot(fig)
+
 if __name__ == "__main__":
     main()
